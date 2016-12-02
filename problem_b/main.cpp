936970871d06c8fbbbd3849abff22c9d076e3af3
puRe <purehdx1337@gmail.com>
2016-12-02 20:19:33 +0100

    updated to gay c++

--- problem_b/main.cpp
+++ problem_b/main.cpp
@@ -13,24 +13,30 @@ int main( ) {
 		if ( line_str.compare( "stop" ) == 0 )
 			break;
 
-		if ( presents.size( ) == 0 )
-			presents.push_back( std::make_pair( line_str, 0 ) );
+		if ( presents.size( ) == 0 ) {
+			presents.push_back( std::make_pair( line_str, 1 ) );
+			continue;
+		}
 
+		bool found = false;
+		std::vector< std::pair< std::string, int > >::iterator asdf_it;
 		for ( std::vector< std::pair< std::string, int > >::iterator it = presents.begin( ); it != presents.end( ); ++it ) {
 			if ( it->first.compare( line_str ) == 0 ) {
-				it->second++;
-				break;
-			}
-			else {
-				presents.push_back( std::make_pair( line_str, 1 ) );
+				found = true;
+				asdf_it = it;
 				break;
 			}
 		}
+		if ( found )
+			asdf_it->second++;
+		else
+			presents.push_back( std::make_pair( line_str, 1 ) );
 	}
 
 	for ( std::vector< std::pair< std::string, int > >::iterator it = presents.begin( ); it != presents.end( ); ++it ) {
 		std::string str = it->second > 1 ? it->first + "s" : it->first;
 		printf( "%d %s\n", it->second, str.c_str( ) );
 	}
+	std::cin.get( );
 	return 0;
 }
