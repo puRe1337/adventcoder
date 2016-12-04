7599d9bd8ff7d2cf353cb5c37cfd29d56fa51d52
florentinaigner <florentin.aigner@gmail.com>
2016-12-04 13:39:40 +0100

    update on problem_d in java

---
+++ problem_d/README.md
@@ -0,0 +1,2 @@
+# Problem D (4th Dec)
+![problem](https://x-up.ws/i/0f2e87daa26c.png)
--- problem_d/problem_d.java
+++ problem_d/problem_d.java
@@ -1,7 +1,51 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-/**
- * Created by wejn on 04.12.2016.
- */
 public class problem_d {
+    public static void main(String[] args) throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        int size = 0;
+        while((s=in.readLine()) != null){
+            size = Integer.parseInt(s);
+            if(true){
+                break;
+            }
+        }
+        for ( int i = 0; i < size; i++ ) {
+            for ( int j = 0; j < i - 1; j++ ) {
+                printSpace( size - j - 1 );
+                printRaute( 3 + ( j * 2 ) );
+                printNewLine( 1 );
+            }
+            printSpace( size - i );
+            printRaute( ( i * 2 ) == 0 ? 1 : i * 2 + 1 );
+            printNewLine( 1 );
+            printSpace( size - 1 - i );
+            printRaute( 3 + ( i * 2 ) );
+            printNewLine( 1 );
+            printSpace( size );
+            printRaute( 1 );
+            printNewLine( 1 );
+        }
+    }
+
+    public static void printRaute(int count){
+        for(int i = 0; i < count; i++){
+            System.out.print("#");
+        }
+    }
+
+    public static void printSpace(int count){
+        for(int i = 0; i < count; i++){
+            System.out.print(" ");
+        }
+    }
+
+    public static void printNewLine(int count){
+        for(int i = 0; i < count; i++){
+            System.out.print("\n");
+        }
+    }
 }
