5d4cce41ff720238a798f6baab859fc3090d46e9
puRe <purehdx1337@gmail.com>
2016-12-05 23:53:17 +0100

    changed parsing

--- problem_e/problem_e.cpp
+++ problem_e/problem_e.cpp
@@ -5,55 +5,61 @@
 #include <vector>
 #include <sstream>
 
-struct stSimpleParse
+struct stShopInfo
 {
-	std::string duration;
-	std::string first_time;
-	std::string second_time;
+	stShopInfo( ) : durationMinutes( -1 ), durationSeconds( -1 ), firstOpenHour( -1 ), firstOpenMinute( -1 ), firstCloseHour( -1 ), firstCloseMinute( -1 ), secondOpenHour( -1 ), secondOpenMinute( -1 ), secondCloseHour( -1 ), secondCloseMinute( -1 ) {}
+
+	int durationMinutes;
+	int durationSeconds;
+
+	int firstOpenHour;
+	int firstOpenMinute;
+	int firstCloseHour;
+	int firstCloseMinute;
+
+	int secondOpenHour;
+	int secondOpenMinute;
+	int secondCloseHour;
+	int secondCloseMinute;
 };
 
-void split( const std::string& s, char delim, std::vector< std::string >& elems ) {
-	std::stringstream ss;
-	ss.str( s );
-	std::string item;
-	while ( std::getline( ss, item, delim ) ) {
-		elems.push_back( item );
-	}
-}
-
-std::vector< std::string > split( const std::string& s, char delim ) {
-	std::vector< std::string > elems;
-	split( s, delim, elems );
-	return elems;
-}
-
 int main( ) {
+	const int TRAVEL_TIME = 20;
 	std::string line_str;
 	std::getline( std::cin, line_str );
 	int amountOfShops = atoi( line_str.c_str( ) );
-	std::vector<stSimpleParse> v_simpleparse;
+	std::vector< stShopInfo > v_shopinfo;
 	for ( int i = 0; i < amountOfShops; i++ ) {
 		line_str.clear( );
 		std::getline( std::cin, line_str );
 
-		std::vector< std::string > duration = split( line_str, ';' );
-		std::vector<std::string> times;
-		if ( duration.at( 1 ).find_first_of( ' ' ) != std::string::npos )
-			times = split( duration.at( 1 ), ' ' );
-		else
-			times.push_back( duration.at( 1 ) );
+		if ( line_str.length( ) > 16 ) {
+			stShopInfo shop_info;
+			sscanf_s( line_str.c_str( ), "%d:%d;%d:%d-%d:%d %d:%d-%d:%d",
+			          &shop_info.durationMinutes, &shop_info.durationSeconds,
+			          &shop_info.firstOpenHour, &shop_info.firstOpenMinute,
+			          &shop_info.firstCloseHour, &shop_info.firstCloseMinute,
+			          &shop_info.secondOpenHour, &shop_info.secondOpenMinute,
+			          &shop_info.secondCloseHour, &shop_info.secondCloseMinute );
+			v_shopinfo.push_back( shop_info );
+		}
+		else {
+			stShopInfo shop_info;
+			sscanf_s( line_str.c_str( ), "%d:%d;%d:%d-%d:%d",
+			          &shop_info.durationMinutes, &shop_info.durationSeconds,
+			          &shop_info.firstOpenHour, &shop_info.firstOpenMinute,
+			          &shop_info.firstCloseHour, &shop_info.firstCloseMinute );
+			v_shopinfo.push_back( shop_info );
 
-		stSimpleParse simple_parse;
-		simple_parse.duration = duration.at( 0 );
-		simple_parse.first_time = times.at( 0 );
-		if ( times.size( ) > 1 )
-			simple_parse.second_time = times.at( 1 );
-		else
-			simple_parse.second_time = "error"; 
-		v_simpleparse.push_back( simple_parse );
+		}
 	}
-	for ( auto parse : v_simpleparse )
-		std::cout << parse.duration << ";" << parse.first_time << " " << parse.second_time << std::endl;
+	for ( auto shop_info : v_shopinfo )
+		printf( "%d:%02d;%d:%02d-%d-%02d %d:%02d-%d-%02d\n",
+		        shop_info.durationMinutes, shop_info.durationSeconds,
+		        shop_info.firstOpenHour, shop_info.firstOpenMinute,
+		        shop_info.firstCloseHour, shop_info.firstCloseMinute,
+		        shop_info.secondOpenHour, shop_info.secondOpenMinute,
+		        shop_info.secondCloseHour, shop_info.secondCloseMinute );
 	std::cin.get( );
 	return 0;
 }
