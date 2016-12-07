c3a410392e729a35d8ab306c5469c142b585ba98
puRe <purehdx1337@gmail.com>
2016-12-08 00:37:54 +0100

    added problem_g in cpp (not done yet)

---
+++ problem_g/problem_g.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+void split( std::string& s, char find, std::vector< std::string >& elements ) {
+	std::stringstream ss( s );
+	std::string item;
+	while ( getline( ss, item, find ) ) {
+		elements.push_back( item );
+	}
+}
+
+std::vector< std::string > split( std::string& s, char find ) {
+	std::vector< std::string > elements;
+	split( s, find, elements );
+	return elements;
+}
+
+
+int main( ) {
+	std::string line_str;
+	std::getline( std::cin, line_str );
+	std::vector< std::string > splitted = split( line_str, ' ' );
+
+
+	for ( int i = 0; i < splitted.size( ); i++ ) {
+		std::string cur_str = splitted.at( i );
+		for ( int j = 0; j < cur_str.size( ); j++ ) {
+			if ( isspace( cur_str.at( j ) ) && isspace( cur_str.at( j + 1 ) ) )
+				cur_str.erase( cur_str.begin( ) + j + 1 );
+			if ( j != 0 && isupper( cur_str.at( j ) ) )
+				cur_str.at( j ) = tolower( cur_str.at( j ) );
+		}
+		splitted.at( i ) = cur_str;
+	}
+	std::string out_str;
+	for ( auto lul : splitted )
+	{
+		if ( lul.find_first_not_of( ' ' ) != std::string::npos ) {
+			lul != splitted.back( ) ? out_str += lul += std::string( " " ) : out_str += lul;
+		}
+	}
+	for ( int i = 0; i < out_str.size( ); i++ ) {
+		if ( out_str.at( i ) == '.' || out_str.at( i ) == '!' || out_str.at( i ) == '?' )
+			out_str.insert( i, "hallo" );
+	}
+	std::cout << out_str << std::endl;
+	std::cin.get( );
+	return 0;
+}
