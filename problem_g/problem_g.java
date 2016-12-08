cafbab78600729dca412e71fb4fab71f1b73a6fe
florentinaigner <florentin.aigner@gmail.com>
2016-12-08 03:34:43 +0100

    @puRe1337 dome die groß klein scheiße wenn das ganze Wort groß is geht nicht, schaus dir bei gelegenheit pls mal an

--- problem_g/problem_g.java
+++ problem_g/problem_g.java
@@ -8,6 +8,7 @@ public class problem_g {
     public static void main(String[] args) throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String s;
+        String result = "";
         while((s = in.readLine()) != null){
             if(s.isEmpty()){
                 break;
@@ -16,7 +17,6 @@ public class problem_g {
             for(char c : s.toCharArray()){
                 chars.add(c);
             }
-            System.out.println(chars);
             for(int i = 0; i < chars.size(); i++){
                 for(int j = 0; j < chars.size(); j++){
                     if(chars.get(j).equals(' ') && chars.get(j+1).equals(' ')){
@@ -24,11 +24,65 @@ public class problem_g {
                     }
                 }
             }
-            String result = "";
-            for(char c: chars){
-                result+=c;
+            StringBuilder builder = new StringBuilder(chars.size());
+            for(Character ch : chars){
+                builder.append(ch);
             }
-            System.out.println(result);
+            String temp = builder.toString();
+            String[] words = temp.split(" ");
+            boolean caps = false;
+            boolean capsFlag = false;
+            for(int i = 0; i < words.length; i++){
+                char[] currentWord = words[i].toCharArray();
+                for(int j = 0; j < currentWord.length; j++){
+                    if(Character.isUpperCase(currentWord[j]) && j != 0){
+                        if(capsFlag == true){
+                            result+=currentWord[j];
+                            continue;
+                        }
+                        int cnt = 0;
+                        for(int k = 0; k < words[i].length(); k++){
+                            if(Character.isUpperCase(words[i].charAt(k))){
+                                cnt++;
+                            }
+                        }
+                        if(cnt == words[i].length()){
+                            result+=currentWord[j];
+                            capsFlag = true;
+                            continue;
+                        }else{
+                            capsFlag = false;
+                            result+=Character.toLowerCase(currentWord[j]);
+                            continue;
+                        }
+                    }else if(j == 0){
+                        result += currentWord[j];
+                        continue;
+                    }
+                    if(caps){
+                        result+= Character.toUpperCase(currentWord[j]);
+                        caps = false;
+                        continue;
+                    }
+                    if(currentWord[j] == '!' || currentWord[j] == '.' || currentWord[j] == '?'){
+                        if(j == currentWord.length-1){
+                            System.out.println("test" + " " + currentWord[j]);
+                            result+=currentWord[j];
+                            continue;
+                        }
+                        else if(currentWord[j+1] != ' '){
+                            result+=currentWord[j] + " ";
+                            caps = true;
+                            continue;
+                        }
+                    }
+                    result+=currentWord[j];
+                }
+                capsFlag = false;
+                result+= " ";
+            }
+            result+="\n";
         }
+        System.out.println(result);
     }
 }
