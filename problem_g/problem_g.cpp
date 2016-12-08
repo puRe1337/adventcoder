25dcb0469f25c3f04e4940f94284be8bf476fd4c
puRe <purehdx1337@gmail.com>
2016-12-08 18:20:53 +0100

    finishes problem g

--- problem_g/problem_g.cpp
+++ problem_g/problem_g.cpp
@@ -18,43 +18,62 @@ std::vector< std::string > split( std::string& s, char find ) {
 }
 
 bool is_upper( const std::string& str ) {
-	int upper = 0;
+	size_t upper = 0;
 	for ( size_t i = 0; i < str.size( ); i++ ) {
 		if ( isupper( str.at( i ) ) )
 			upper++;
+		else if ( !isalpha( str.at( i ) ) )
+			upper++;
 	}
-	return upper == str.size(  );
+	return upper == str.size( );
 }
 
+bool is_space( const std::string& str ) {
+	size_t spaces = 0;
+	for ( size_t i = 0; i < str.size( ); i++ ) {
+		if ( isspace( str.at( i ) ) )
+			spaces++;
+	}
+	return spaces == str.size( );
+}
 
 int main( ) {
 	std::string line_str;
-	std::getline( std::cin, line_str );
-	std::vector< std::string > splitted = split( line_str, ' ' );
+	do {
+		std::getline( std::cin, line_str );
+
+		for ( size_t i = 0; i < line_str.size( ); i++ ) {
+			if ( ( line_str.at( i ) == '.' || line_str.at( i ) == '!' || line_str.at( i ) == '?' ) && i != line_str.size( ) ) {
+				line_str.insert( i + 1, " " );
+				if ( i + 2 > line_str.size( ) )
+					line_str.at( i + 2 ) = toupper( line_str.at( i + 2 ) );
+			}
+		}
+
+		std::vector< std::string > splitted = split( line_str, ' ' );
 
 
-	for ( size_t i = 0; i < splitted.size( ); i++ ) {
-		std::string cur_str = splitted.at( i );
-		for ( size_t j = 0; j < cur_str.size( ); j++ ) {
-			if ( isspace( cur_str.at( j ) ) && isspace( cur_str.at( j + 1 ) ) )
-				cur_str.erase( cur_str.begin( ) + j + 1 );
-			if ( j != 0 && isupper( cur_str.at( j ) ) && !is_upper( cur_str ) )
-				cur_str.at( j ) = tolower( cur_str.at( j ) );
+		for ( size_t i = 0; i < splitted.size( ); i++ ) {
+			std::string cur_str = splitted.at( i );
+			for ( size_t j = 0; j < cur_str.size( ); j++ ) {
+				if ( isspace( cur_str.at( j ) ) && isspace( cur_str.at( j + 1 ) ) )
+					cur_str.erase( cur_str.begin( ) + j + 1 );
+				if ( j != 0 && isupper( cur_str.at( j ) ) && isalpha( cur_str.at( j ) ) && !is_upper( cur_str ) )
+					cur_str.at( j ) = tolower( cur_str.at( j ) );
+			}
+			splitted.at( i ) = cur_str;
 		}
-		splitted.at( i ) = cur_str;
-	}
-	std::string out_str;
-	for ( auto lul : splitted )
-	{
-		if ( lul.find_first_not_of( ' ' ) != std::string::npos ) {
-			lul != splitted.back( ) ? out_str += lul += std::string( " " ) : out_str += lul;
+		std::string out_str;
+		for ( size_t i = 0; i < splitted.size( ); i++ ) {
+			std::string lul = splitted.at( i );
+			if ( lul.find_first_not_of( ' ' ) != std::string::npos ) {
+				lul != splitted.back( ) ? out_str += lul += std::string( " " ) : out_str += lul;
+			}
 		}
+		if ( !out_str.empty( ) && !is_space( out_str ) )
+			std::cout << out_str << std::endl;
 	}
-	for ( size_t i = 0; i < out_str.size( ); i++ ) {
-		if ( out_str.at( i ) == '.' || out_str.at( i ) == '!' || out_str.at( i ) == '?' )
-			out_str.insert( i, "hallo" );
-	}
-	std::cout << out_str << std::endl;
+	while ( !line_str.empty( ) );
 	std::cin.get( );
 	return 0;
 }
