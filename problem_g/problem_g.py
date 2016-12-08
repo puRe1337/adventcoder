1961b53aec4a7ff464693ee455660e99f7cc5bd3
Loeh <simclaphi@yahoo.de>
2016-12-08 02:13:29 +0100

    @wejn1 @puRe1337 bitte drüber schauen kp was falsch ist

--- problem_g/problem_g.py
+++ problem_g/problem_g.py
@@ -10,19 +10,34 @@ while (EnteredString != ""):
 	i=0
 	CorrectedString = ""
 	while (i<len(words)):
+		checkQ = words[i].split("?")
+		checkE = words[i].split("!")
+		checkP = words[i].split(".")
+		if (len(checkQ) > 1 and checkQ[1] != ""):
+			words[i]=checkQ[1]
+			words.insert(i,checkQ[0]+"?")
+		if (len(checkE) > 1 and checkE[1] != ""):
+			words[i]=checkE[1]
+			words.insert(i,checkE[0]+"!")
+		if (len(checkP) > 1 and checkP[1] != ""):
+			words[i]=checkP[1]
+			words.insert(i,checkP[0]+".")
 		if (words[i]==""):
 			i=i+1
 			continue
 		wordLen = len(words[i])-1
-		if (words[i][0].isupper() and words[i][1].islower()):
-			words[i] = words[i].capitalize()
-		elif(words[i].isupper()):
+		#if (words[i][0].isupper() and words[i][1].islower()):
+		#	words[i] = words[i].capitalize()
+		if(words[i].isupper()):
 			words[i] = words[i].upper()
 		else:
 			words[i] = words[i].lower()
 		if(SentenceEnd == 1):
 			SentenceEnd = 0
-			words[i] = words[i].capitalize()
+			if(words[i].isupper()):
+				words[i] = words[i].upper()
+			else:
+				words[i] = words[i].capitalize()
 		if(words[i][wordLen] == "." or words[i][wordLen] == "!" or words[i][wordLen] == "?"):
 			SentenceEnd = 1
 		if(i==len(words)-1):
