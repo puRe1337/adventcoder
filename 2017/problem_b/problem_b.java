ca82004a7ba6312cdf53f745f03791ee9b1daa21
Florentin Aigner <florentin.aigner@gmail.com>
2017-12-02 12:42:02 +0100

    finished problem_b (java)

--- 2017/problem_b/problem_b.java
+++ 2017/problem_b/problem_b.java
@@ -2,11 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class problem_b {
     public static void main(String[] args) throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String s;
+        Map<Integer, String> base = new TreeMap<>();
+        base.put(1, "Friday");
+        base.put(2, "Saturday");
+        base.put(3, "Sunday");
+        base.put(4, "Monday");
+        base.put(5, "Tuesday");
+        base.put(6, "Wednesday");
+        base.put(7, "Thursday");
         ArrayList<Integer> days = new ArrayList<>();
         while(!(s = in.readLine()).equals("end")){
             for(String c : s.split(" ")){
@@ -16,6 +26,12 @@ public class problem_b {
                 days.add(Integer.parseInt(c));
             }
         }
-        System.out.println(days);
+        for(int i : days){
+            if(i == 7){
+                System.out.println(base.get(7));
+                continue;
+            }
+            System.out.println(base.get(i % 7));
+        }
     }
 }
