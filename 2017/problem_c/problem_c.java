d0f32ba1b0643434e32c714b7189f8b0e25ff53c
Florentin Aigner <florentin.aigner@gmail.com>
2017-12-03 19:55:44 +0100

    added problem_c (java) nochmal...

--- 2017/problem_c/problem_c.java
+++ 2017/problem_c/problem_c.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class problem_c {
+    public static void main(String[] args) throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        ArrayList<Integer> candles = new ArrayList<>();
+        ArrayList<Integer> results = new ArrayList<>();
+        while(!(s = in.readLine()).equals("end")){
+            for(int i = 0; i < s.length(); i++){
+                if(s.charAt(i) != ' '){
+                    candles.add((Integer.parseInt(s.charAt(i) + "")));
+                }
+            }
+            int res = 0;
+            for(int i : candles){
+                res+=i*60;
+            }
+            res = res / candles.size();
+            results.add(res);
+            res = 0;
+        }
+        for(int i : results){
+            System.out.println(i);
+        }
+    }
 }
