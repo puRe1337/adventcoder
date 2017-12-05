1d4aa7711896916d27e51b67210aa3e02c0dafac
Florentin Aigner <florentin.aigner@gmail.com>
2017-12-05 22:33:17 +0100

    finished problem_e (java)

--- 2017/problem_e/problem_e.java
+++ 2017/problem_e/problem_e.java
@@ -7,15 +7,22 @@ import java.util.Arrays;
 public class problem_e {
 
     public static void main(String[] args) throws IOException{
-        ArrayList<Character> charBase = new ArrayList<>();
-        charBase.addAll(Arrays.asList('S', 'A', 'N', 'T', 'A', 'C', 'L', 'A', 'U', 'S'));
         ArrayList<Character> temp = new ArrayList<>();
-        temp = charBase;
+        temp.addAll(Arrays.asList('S', 'A', 'N', 'T', 'A', 'C', 'L', 'A', 'U', 'S'));
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String s = in.readLine().toUpperCase();
         in.close();
         String res = "";
+        boolean blockflag = false;
         for(int i = 0; i < s.length(); i++){
+            if(blockflag){
+                if(s.charAt(i) == ' '){
+                    blockflag = false;
+                    continue;
+                }else{
+                    continue;
+                }
+            }
             if(temp.size() == 0){
                 if(res.equals("SANTA CLAUS")){
                     temp.addAll(Arrays.asList('S', 'A', 'N', 'T', 'A', 'C', 'L', 'A', 'U', 'S'));
@@ -27,19 +34,25 @@ public class problem_e {
                     res = "";
                 }
             }
-            if(s.charAt(i) == ' '){
-                res+=' ';
-                continue;
-            }
             if(temp.contains(s.charAt(i))){
                 res+=s.charAt(i);
                 temp.remove(temp.indexOf(s.charAt(i)));
-            }else{
-                res = "";
+            }else if(s.charAt(i) == ' '){
+                res+= ' ';
+            }else if(s.charAt(i) == '.' || s.charAt(i) == ','){
+
             }
+            else{
+                res = "";
+                temp.clear();
+                temp.addAll(Arrays.asList('S', 'A', 'N', 'T', 'A', 'C', 'L', 'A', 'U', 'S'));
+                blockflag = true;
+            }
+        }
+        if(temp.size() == 0){
+            System.out.println(res);
         }
 
     }
-
 
 }
